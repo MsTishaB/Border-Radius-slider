a2d3d273afbf6d91e4185ae94f0c2507bbf6c2fc
MsTishaB <98257816+MsTishaB@users.noreply.github.com>
2022-08-09 14:08:56 -0400

    Border radius now updates on green box

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.getElementById("slider").addEventListener("change", function () {
 	let newValue = slider.value;
 	document.getElementById("borderRadius").innerHTML = newValue + "%";
+	document.getElementById("box").style.borderRadius = `${newValue}px`;
 
-	console.log(document.getElementById("box"));
+	console.log(document.getElementById("box").style.borderRadius);
 });
